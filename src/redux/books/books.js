c6b4d118c5cb7e9d600fec56cc036092fb944f55
amklass11 <lassanaa14@gmail.com>
2023-03-04 09:04:47 +0100

    coonect api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,38 +1,37 @@
-import { v4 as uuidv4 } from 'uuid';
+import { apiAddBook, apiGetBooks, apiRemoveBook } from '../../modules/api';
 
 const ADD = 'bookstore/Book/ADD';
+const GET = 'bookstore/Book/GET';
 const REMOVE = 'bookstore/Book/REMOVE';
 
-const initialState = [
-  {
-    id: uuidv4(),
-    title: 'The Hunger Game',
-    author: 'Suzanne Collins',
-  },
-  {
-    id: uuidv4(),
-    title: 'Dune',
-    author: 'Frank Herbert',
-  },
-  {
-    id: uuidv4(),
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-  },
-];
+export const addBook = (payload) => async (dispatch) => {
+  const book = { ...payload, item_id: payload.id, category: 'category' };
+  await apiAddBook(book);
+  dispatch({ type: ADD, book });
+};
 
-export function addBook(book) {
-  return { type: ADD, book };
-}
+export const getBooks = () => async (dispatch) => {
+  const data = await apiGetBooks();
+  const books = Object.keys(data).map((key) => {
+    const book = data[key][0];
+    book.id = key;
+    return book;
+  });
+  dispatch({ type: GET, books });
+};
 
-export function removeBook(id) {
-  return { type: REMOVE, id };
-}
+export const removeBook = (id) => async (dispatch) => {
+  await apiRemoveBook(id);
+  dispatch({ type: REMOVE, id });
+};
 
-export default function reducer(state = initialState, action = {}) {
+export default function reducer(state = [], action = {}) {
   switch (action.type) {
     case ADD:
       return [...state, action.book];
+
+    case GET:
+      return action.books;
 
     case REMOVE:
       return [...state].filter((book) => book.id !== action.id);
